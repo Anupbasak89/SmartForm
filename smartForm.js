a4c409626d942f9b15aa2bcb84ce8321c1057739
cis101 <cis101@secs.iub.edu.bd>
2017-06-11 10:55:12 +0600

    Assignment 08

--- smartForm.js
+++ smartForm.js
@@ -2,16 +2,16 @@
 
 var userData = {
 	name: "",
-	email: "";
+	email: "",
 	html: {likes:[], dislikes:[]},
 	css: {likes:[], dislikes:[]},
 	js: {likes:[], dislikes:[]},
-	skills: {html:"", css: "", js: ""}
-
-
-
+	skills: {html:"", css:"", js:""} ,
+	surveyState: 0 
 };
-
+if ( window.localStorage.userData) {
+	userData =JSON.parse(window.localStorage.userData);
+}
 
 $("#starBtn").click(function(event){
 console.log("start clicked")
@@ -22,12 +22,23 @@ $("#q1").show();
 
 $("#name").change(function(event){
 console.log($("#name").val());
-if($("name").val()){
+if($("#name").val()){
 	userData.name = $("#name").val();
+	console.log(window.localStorage.userData);
+	window.localStorage.userData = JSON.stringify(userData);
 }
 });
 
 $("#email").change(function(event){
+	console.log($("#email").val());
+	if ($("#email").val()){
+		userData.email = $("#email").val();
+		console.log(window.localStorage.userData);
+		window.localStorage.userData = JSON.stringify(userData);
+}
+
+
+
 
 
 });
